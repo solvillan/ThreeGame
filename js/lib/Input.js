1303bd5e208bd62f2b6030fdc12865fa423b320f
Rickard Doverfelt <rickard.doverfelt@gmail.com>
2017-02-15 19:04:15 +0100

    Some actual GAME!

---
+++ js/lib/Input.js
@@ -0,0 +1,63 @@
+/**
+ * Inspired by http://nokarma.org/2011/02/27/javascript-game-development-keyboard-input/
+ * @type {{_pressed: {}, LEFT: number, RIGHT: number, UP: number, DOWN: number, isDown: Key.isDown, onKeyDown: Key.onKeyDown, onKeyUp: Key.onKeyUp}}
+ */
+
+var Key = {
+    _pressed: {},
+
+    LEFT: 65,
+    RIGHT: 68,
+    UP: 87,
+    DOWN: 83,
+
+    isDown: function (keyCode) {
+        return this._pressed[keyCode];
+    },
+
+    onKeyDown: function (event) {
+        this._pressed[event.keyCode] = true;
+    },
+
+    onKeyUp: function(event) {
+        delete this._pressed[event.keyCode];
+    }
+};
+
+var Mouse = {
+    _position: {x: 0, y: 0},
+    _delta: {x: 0, y: 0},
+
+    getPosition: function () {
+        return this._position;
+    },
+
+    getX: function () {
+        return this._position.x;
+    },
+
+    getY: function () {
+        return this._position.y;
+    },
+
+    getDelta: function () {
+        return this._delta;
+    },
+
+    getDX: function () {
+        return this._delta.x;
+    },
+
+    getDY: function () {
+        return this._delta.y;
+    },
+
+    update: function (canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        this._position.x = evt.clientX - rect.left;
+        this._position.y =  evt.clientY - rect.top;
+        this._delta.x = evt.movementX;
+        this._delta.y = evt.movementY;
+    }
+
+};
--- js/main.js
+++ js/main.js
@@ -5,23 +5,92 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var cube = new Cube(0, 0, 0, 1, 1, 1, 0x00ff00);
+renderer.domElement.requestPointerLock = renderer.domElement.requestPointerLock ||
+    renderer.domElement.mozRequestPointerLock;
 
-scene.add( cube.mesh );
+var lastMousePos = {};
 
-geometry = new THREE.PlaneGeometry(5, 5, 3, 3);
-material = new THREE.MeshBasicMaterial( { color: 0xcecece } );
-var plane = new THREE.Mesh(geometry, material);
-plane.position.z = -2;
-scene.add(plane);
+var player = new Cube(0, 0.5, 0, 1, 1, 1, 0x00ff00);
+
+scene.add( player.mesh );
+
+var plane = new Plane(0, 0, 0, 40, 40, 0x227722);
+plane.mesh.rotation.x = -Math.PI/2;
+scene.add(plane.mesh);
+
+var enemies = [];
+var bullets = [];
 
 camera.position.z = 5;
 
+function addEnemy() {
+    enemies[enemies.length] = new Enemy((Math.random()-0.5)*40, (Math.random()-0.5)*40);
+    scene.add(enemies[enemies.length-1]._cube.mesh);
+}
+
+function shoot() {
+
+}
+
 function render() {
     requestAnimationFrame(render);
-    cube.mesh.rotation.x += 0.05;
-    cube.mesh.rotation.y += 0.05;
+    update();
     renderer.render(scene, camera);
 }
+
+function update() {
+    if (Key.isDown(Key.UP)) {
+        player.mesh.translateZ(-0.1);
+    }
+    if (Key.isDown(Key.DOWN)) {
+        player.mesh.translateZ(0.1);
+    }
+    if (Key.isDown(Key.RIGHT)) {
+        player.mesh.translateX(0.1);
+    }
+    if (Key.isDown(Key.LEFT)) {
+        player.mesh.translateX(-0.1);
+    }
+    if (Math.random() < 0.1 && enemies.length < 20) {
+        addEnemy();
+    }
+    for (var i = 0; i < enemies.length; i++) {
+        enemies[i].update();
+    }
+
+    camera.position.set(player.mesh.position.x, player.mesh.position.y + 1, player.mesh.position.z);
+    //camera.rotation.x = -calcRotation(Mouse.getY(), window.innerHeight);
+}
+
+function calcRotation(delta, max) {
+    return (delta/max)*Math.PI;
+}
+
+window.addEventListener('keydown', function (event) {
+    Key.onKeyDown(event);
+});
+
+window.addEventListener('keyup', function (event) {
+    Key.onKeyUp(event);
+});
+
+function updatePos() {
+    player.mesh.rotation.y -= calcRotation(Mouse.getDX(), window.innerWidth);
+    player.mesh.updateMatrix();
+    camera.rotation.y = player.mesh.rotation.y;
+    //camera.rotation.x += calcRotation(Mouse.getDY(), window.innerHeight);
+}
+document.addEventListener('mousemove', function (event) {
+    lastMousePos = Mouse.getPosition();
+    Mouse.update(renderer.domElement, event);
+    updatePos();
+    return false;
+});
+
+renderer.domElement.addEventListener('click', function (event) {
+    renderer.domElement.requestPointerLock();
+});
+
+lastMousePos = Mouse.getPosition();
 
 render();
